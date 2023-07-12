39aef717044d3980b1ca48dbf7fc1ef46cb7760f
buhleqampi <buhle3857@gmail.com>
2023-07-12 13:37:33 +0200

    second

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 // // DELETE THE CLIENT'S NAME
-function deleteClientName (emailBody) {
-    let client =  /Herr Krause[^, \n]*/;
-    emailBody = emailBody.replace(client, '');
+function deleteClientName(emailBody) {
+  let client = /Herr Krause[^, \n]*/;
+  emailBody = emailBody.replace(client, "");
 
-    let person = /Werner Muster[^, \n]*/;
-    emailBody = emailBody.replace(person, '');
+  let person = /Werner Muster[^, \n]*/;
+  emailBody = emailBody.replace(person, "");
 
+  if (
+    emailBody.includes("Herr Krause") ||
+    emailBody.includes("Werner Muster")
+  ) {
+    throw new Error("Client name not properly removed from email body.");
+  }
 
-    if (emailBody.includes('Herr Krause') || emailBody.includes('Werner Muster')) {
-        throw new Error('Client name not properly removed from email body.');
-      }
-    
-    return emailBody;
-    }
-
-    const emailBody = "Sehr geehrter Herr Krause \n können Sie mir einen aktuellen Sachstand in meiner Angelegenheit geben? Wie geht es weiter. \n Liebe Grüße \n Werner Muster"
-    
-    try {
-    const newEmailBody = deleteClientName(emailBody);
-    // debugger
-    console.log(newEmailBody);
-} catch(error) {
-    console.error ('Error:',error.message);
+  return emailBody;
 }
+debugger
+const emailBody =
+  "Sehr geehrter Herr Krause \n können Sie mir einen aktuellen Sachstand in meiner Angelegenheit geben? Wie geht es weiter. \n Liebe Grüße \n Werner Muster";
 
+try {
+  const newEmailBody = deleteClientName(emailBody);
+  console.log(newEmailBody);
+} catch (error) {
+  console.error("Error:", error.message);
+}
