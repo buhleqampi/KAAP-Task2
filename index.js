fc2a84228f65f1fba2b44b45bfacffaa3d488dc2
buhleqampi <buhle3857@gmail.com>
2023-07-12 13:14:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// // DELETE THE CLIENT'S NAME
+function deleteClientName (emailBody) {
+    let client =  /Herr Krause[^, \n]*/;
+    emailBody = emailBody.replace(client, '');
+
+    let person = /Werner Muster[^, \n]*/;
+    emailBody = emailBody.replace(person, '');
+
+
+    if (emailBody.includes('Herr Krause') || emailBody.includes('Werner Muster')) {
+        throw new Error('Client name not properly removed from email body.');
+      }
+    
+    return emailBody;
+    }
+
+    const emailBody = "Sehr geehrter Herr Krause \n können Sie mir einen aktuellen Sachstand in meiner Angelegenheit geben? Wie geht es weiter. \n Liebe Grüße \n Werner Muster"
+    
+    try {
+    const newEmailBody = deleteClientName(emailBody);
+    // debugger
+    console.log(newEmailBody);
+} catch(error) {
+    console.error ('Error:',error.message);
+}
+
