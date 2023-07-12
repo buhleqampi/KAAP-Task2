f4fb8626e8eb099dd4377221d869644901d47597
buhleqampi <buhle3857@gmail.com>
2023-07-12 17:00:49 +0200

    third

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // // DELETE THE CLIENT'S NAME
 function deleteClientName(emailBody) {
-  let client = /Herr Krause[^, \n]*/;
-  emailBody = emailBody.replace(client, "");
+  let salutationRegex = /Sehr geehrter Herr[^,\n]*/;
+  emailBody = emailBody.replace(salutationRegex, '');
 
-  let person = /Werner Muster[^, \n]*/;
-  emailBody = emailBody.replace(person, "");
+  let greetingRegex = /Mit freundlichen Grüßen[^,\n]*/;
+  emailBody = emailBody.replace(greetingRegex, '');
 
   if (
-    emailBody.includes("Herr Krause") ||
-    emailBody.includes("Werner Muster")
+    emailBody.includes("Sehr geehrter Herr") ||
+    emailBody.includes("Mit freundlichen Grüßen")
   ) {
     throw new Error("Client name not properly removed from email body.");
   }
@@ -17,7 +17,7 @@ function deleteClientName(emailBody) {
 }
 debugger
 const emailBody =
-  "Sehr geehrter Herr Krause \n können Sie mir einen aktuellen Sachstand in meiner Angelegenheit geben? Wie geht es weiter. \n Liebe Grüße \n Werner Muster";
+"Sehr geehrter Herr Qampi,\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\n\nMit freundlichen Grüßen,\nBuhle Qampi";
 
 try {
   const newEmailBody = deleteClientName(emailBody);
@@ -25,3 +25,4 @@ try {
 } catch (error) {
   console.error("Error:", error.message);
 }
+  
